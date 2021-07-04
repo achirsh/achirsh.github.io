a764633467d9ea2df338224a72c093d6e9d68132
achirsh <1148413781@qq.com>
2021-07-04 14:55:20 +0800

    Update node.js

--- videos/node.js
+++ videos/node.js
@@ -1,25 +1,3 @@
-// var express = require('express')
-// var app = express();
-// var fs = require('fs')
-// var http = require('http').createServer(app);
-// app.get('/send', async(req, res) => {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-
-//     function read() {
-//         return new Promise((resolve, rejects) => {
-//             fs.readFile('./1.jpg', function(err, data) {
-//                 resolve(data)
-//                 rejects(err)
-//             })
-//         })
-//     }
-//     let data = await read()
-//     res.send(data)
-// })
-// http.listen(8888, () => {
-//     console.log('listening on *:8888');
-// });
-
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
@@ -33,26 +11,11 @@ var server = http.createServer(function(req, res) {
     });
 
     var path = url.parse(req.url, true);
-    if (path.pathname == '/index') {
-        const video = fs.readFileSync('./demo.mp4');
-        const length = video.length;
-        let result = '';
-        for (let i = 0; i < length; i++) {
-            const binStr = Number(video[i]).toString(2);
-            result += '0'.repeat(8 - binStr.length) + binStr;
-        }
-
-        const demo = fs.readFileSync('./')
-
-        res.write(result.toString());
-        res.end();
-    }
 
     if (path.pathname == '/medium.m3u8') {
         const { key } = path.query
         const value = Buffer.from(key, 'base64').toString('utf-8')
         if (value == 'hello') {
-            const file = fs.readFileSync('./demo.m3u8');
             res.write(Buffer.from('https://achirsh.github.io/videos/demo.m3u8').toString('base64'));
         } else {
             res.write('');
